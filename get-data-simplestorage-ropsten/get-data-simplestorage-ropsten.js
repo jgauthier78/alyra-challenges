5b3442ed455e678cb411f6ed639754512774b444
Jérôme Gauthier <jgauthier@cpaglobal.com>
2021-06-10 08:37:32 +0200

    Ajout de différents exercices dapp - fix sur l'url infura pour get-data-simplestorage-ropsten

--- get-data-simplestorage-ropsten/get-data-simplestorage-ropsten.js
+++ get-data-simplestorage-ropsten/get-data-simplestorage-ropsten.js
@@ -1,5 +1,5 @@
 var  Web3  =  require('web3');
-web3  =  new  Web3(new  Web3.providers.HttpProvider('https://mainnet.ropsten.io/v3/b717bccc90a842da87cca894a7e5ec59'));
+web3  =  new  Web3(new  Web3.providers.HttpProvider('https://ropsten.infura.io/v3/b717bccc90a842da87cca894a7e5ec59'));
 
 var  abi  = [{"inputs": [],"name": "get","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "uint256","name": "x","type": "uint256"}],"name": "set","outputs": [],"stateMutability": "nonpayable","type": "function"}]
 var  contract  =  new  web3.eth.Contract(abi, '0x8cD906ff391b25304E0572b92028bE24eC1eABFb');
