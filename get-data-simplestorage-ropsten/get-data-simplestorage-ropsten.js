0c97bc7e81638efbefc6a1c7ec4a0ff38be00359
Jérôme Gauthier <jgauthier@cpaglobal.com>
2021-06-10 08:28:54 +0200

    Ajout de différents exercices dapp - simple-storage-truffle-deploy-ropsten - simple-storage-truffle-deploy-rinkeby - get-data-simplestorage-ropsten

---
+++ get-data-simplestorage-ropsten/get-data-simplestorage-ropsten.js
@@ -0,0 +1,18 @@
+var  Web3  =  require('web3');
+web3  =  new  Web3(new  Web3.providers.HttpProvider('https://mainnet.ropsten.io/v3/b717bccc90a842da87cca894a7e5ec59'));
+
+var  abi  = [{"inputs": [],"name": "get","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "uint256","name": "x","type": "uint256"}],"name": "set","outputs": [],"stateMutability": "nonpayable","type": "function"}]
+var  contract  =  new  web3.eth.Contract(abi, '0x8cD906ff391b25304E0572b92028bE24eC1eABFb');
+
+console.log('------------------------------------------');
+console.log('Get smart contract data from contract:');
+console.log('0x8cD906ff391b25304E0572b92028bE24eC1eABFb');
+console.log('------------------------------------------');
+
+contract.methods.get().call()
+.then(function(result){
+   console.log('');
+   console.log('data:'+result);
+}).catch(function(error){
+   console.log('error:'+error);
+});
---
+++ projet-truffle/simple-storage-truffle-deploy-rinkeby/truffle-config.js
@@ -0,0 +1,61 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+require('dotenv').config()
+const mnemonic = process.env.MNEMONIC;
+const infuraUrl = process.env.INFURA_URL;
+const networkId = process.env.NETWORK_ID;
+
+// deployed contract address: 0x10B48805DC1082405d8253E59362Bc3A1e5ADF69
+
+module.exports = {
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic,infuraUrl),
+      network_id: networkId,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.6.11",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
+       evmVersion: "byzantium"
+      }
+    }
+  },
+
+  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
+  //
+  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
+  // those previously migrated contracts available in the .db directory, you will need to run the following:
+  // $ truffle migrate --reset --compile-all
+
+  db: {
+    enabled: false
+  }
+};
